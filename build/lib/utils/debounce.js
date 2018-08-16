73f2929704b0e32f54079ce379f80b414b446934
Rob Zyskowski <zyskowski.rob@gmail.com>
2018-08-16 14:37:53 +0200

    update build

--- build/lib/utils/debounce.js
+++ build/lib/utils/debounce.js
@@ -1,12 +1,20 @@
-export default (func, wait, immediate) => {
-    let timeout;
-    return () => {
-        let context = this, args = arguments;
-        let later = () => {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var _arguments = arguments;
+
+exports.default = function (func, wait, immediate) {
+    var timeout = void 0;
+    return function () {
+        var context = undefined,
+            args = _arguments;
+        var later = function later() {
             timeout = null;
             if (!immediate) func.apply(context, args);
         };
-        let callNow = immediate && !timeout;
+        var callNow = immediate && !timeout;
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
--- build/lib/utils/transformProperty.js
+++ build/lib/utils/transformProperty.js
@@ -1,7 +1,13 @@
-export default (() => {
-    const transform = typeof window !== 'undefined' && window.document.documentElement.style.transform;
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+exports.default = function () {
+    var transform = typeof window !== 'undefined' && window.document.documentElement.style.transform;
     if (typeof transform === 'string') {
         return 'transform';
     }
     return 'WebkitTransform';
-})();
+}();
